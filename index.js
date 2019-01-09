63bcf834a46b0724a8fd44c3d65a23943012b55b
Dan Kantor <dan.kantor@gmail.com>
2019-01-09 10:21:40 -0500

    removed jQuery and bumped version to 1.0

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 module.exports.get = function(key){
-    return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(localStorage.getItem(key));
 };
 
 module.exports.set = function(key, value){
-    try {
-        localStorage.setItem(key, JSON.stringify(value));
-    }
-    catch(e){
-        if(e.name === 'QUOTA_EXCEEDED_ERR') {
-            $(window).trigger(
-                'storageQuotaExceeded',
-                e
-            );
-        }
-    }
+  localStorage.setItem(key, JSON.stringify(value));
 };
 
 module.exports.remove = function(key){
-    localStorage.removeItem(key);
+  localStorage.removeItem(key);
 };
 
 module.exports.clear = function(){
-    localStorage.clear();
+  localStorage.clear();
 };
 
